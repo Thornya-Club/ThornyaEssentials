a50b8a8696ff6b5f1585c6d736b339e341a691ff
Gusttavo <gustavosousa918@gmail.com>
2022-06-25 18:57:54 -0300

    Initial commit

---
+++ src/main/java/thornyaessentials/thornyaessentials/ThornyaEssentials.java
@@ -0,0 +1,17 @@
+package thornyaessentials.thornyaessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThornyaEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
