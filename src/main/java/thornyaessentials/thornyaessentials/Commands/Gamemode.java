71860cc5f6d9ec71de053a314287c949123563b4
Gusttavo <gustavosousa918@gmail.com>
2022-06-25 21:46:06 -0300

    Added Gamemode Command Permission and Created Messages Default Class

--- src/main/java/thornyaessentials/thornyaessentials/Commands/Gamemode.java
+++ src/main/java/thornyaessentials/thornyaessentials/Commands/Gamemode.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import thornyaessentials.thornyaessentials.Utils.Messages;
 
 public class Gamemode implements CommandExecutor {
     @Override
@@ -13,29 +14,52 @@ public class Gamemode implements CommandExecutor {
 
         if(!(snd instanceof Player)){
             //Console dar gm para jogadores
+            if(args.length == 2){
+                if(Bukkit.getPlayer(args[1]) != null) {
+                    if (Bukkit.getPlayer(args[1]).isOnline()) {
+                        Player target = Bukkit.getPlayer(args[1]);
+                        changeGamemodePlayer(target, args[0], false);
+                        Bukkit.getConsoleSender().sendMessage("§aVocê mudou o gamemode de §c" + target.getDisplayName());
+                    } else {
+                        Bukkit.getConsoleSender().sendMessage("§cJogador não existe.");
+                    }
+                }else {
+                    Bukkit.getConsoleSender().sendMessage("§cJogador não está online.");
+                }
+            }else {
+                Bukkit.getConsoleSender().sendMessage("§cUse /gamemode (gamemode) (player)");
+            }
         }else {
             if(cmd.getName().equalsIgnoreCase("gamemode") || cmd.getName().equalsIgnoreCase("gm")){
                 //permission gamemode set
                 Player p = (Player) snd;
-                if(args.length == 0){
-                    switchGamemodePlayer(p);
-                }else if(args.length == 1){
-                    changeGamemodePlayer(p, args[0], true);
-                }else if(args.length == 2){
-                    //permission gamemode others
-                    if(Bukkit.getPlayer(args[1]) != null) {
-                        if (Bukkit.getPlayer(args[1]).isOnline()) {
-                            Player target = Bukkit.getPlayer(args[1]);
-                            changeGamemodePlayer(target, args[0], false);
-                            p.sendMessage("§aVocê mudou o gamemode de §c" + target.getDisplayName());
+                if(p.hasPermission("thornya.essentials.gamemode")) {
+                    if (args.length == 0) {
+                        switchGamemodePlayer(p);
+                    } else if (args.length == 1) {
+                        changeGamemodePlayer(p, args[0], true);
+                    } else if (args.length == 2) {
+                        //permission gamemode others
+                        if (p.hasPermission("thornya.essentials.gamemode.others")) {
+                            if (Bukkit.getPlayer(args[1]) != null) {
+                                if (Bukkit.getPlayer(args[1]).isOnline()) {
+                                    Player target = Bukkit.getPlayer(args[1]);
+                                    changeGamemodePlayer(target, args[0], false);
+                                    p.sendMessage("§aVocê mudou o gamemode de §c" + target.getDisplayName());
+                                } else {
+                                    p.sendMessage("§cJogador não está online.");
+                                }
+                            } else {
+                                p.sendMessage("§cJogador não existe.");
+                            }
                         } else {
-                            p.sendMessage("§cJogador não existe.");
+                            p.sendMessage("§cUse /gm (gamemode) [player]");
                         }
                     }else {
-                        p.sendMessage("§cJogador não está online.");
+                        p.sendMessage(Messages.NO_PERMISSION);
                     }
                 }else{
-                    p.sendMessage("§cUse /gm (gamemode) [player]");
+                    p.sendMessage(Messages.NO_PERMISSION);
                 }
             }
         }
@@ -47,15 +71,19 @@ public class Gamemode implements CommandExecutor {
         GameMode gm;
         //permission gamemodes
         if(gameMode.equalsIgnoreCase("1") || gameMode.equalsIgnoreCase("CREATIVE") || gameMode.equalsIgnoreCase("criativo")){
+            if(!player.hasPermission("thornya.essentials.gamemode.creative")) return false;
             gm = GameMode.CREATIVE;
             if(showMessage) player.sendMessage("§aVocê mudou seu gamemode para o §ccriativo");
         }else if(gameMode.equalsIgnoreCase("0") || gameMode.equalsIgnoreCase("SURVIVAL")){
+            if(!player.hasPermission("thornya.essentials.gamemode.survival")) return false;
             gm = GameMode.SURVIVAL;
             if(showMessage) player.sendMessage("§aVocê mudou seu gamemode para o §csobrevivência");
         }else if(gameMode.equalsIgnoreCase("2") || gameMode.equalsIgnoreCase("ADVENTURE") || gameMode.equalsIgnoreCase("aventura")){
+            if(!player.hasPermission("thornya.essentials.gamemode.adventure")) return false;
             gm = GameMode.ADVENTURE;
             if(showMessage) player.sendMessage("§aVocê mudou seu gamemode para o modo §caventura");
         }else if(gameMode.equalsIgnoreCase("3") || gameMode.equalsIgnoreCase("SPECTATOR") || gameMode.equalsIgnoreCase("espectador")){
+            if(!player.hasPermission("thornya.essentials.gamemode.spectator")) return false;
             gm = GameMode.SPECTATOR;
             if(showMessage) player.sendMessage("§aVocê mudou seu gamemode para o §cespectador");
         } else {
---
+++ src/main/java/thornyaessentials/thornyaessentials/Utils/Messages.java
@@ -0,0 +1,7 @@
+package thornyaessentials.thornyaessentials.Utils;
+
+public class Messages {
+
+    public static String NO_PERMISSION = "§cVocê não tem permissão para usar esse comando.";
+
+}
